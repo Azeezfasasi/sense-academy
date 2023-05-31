0ef51d28ce6f04ae8870cc7c1894236e8eeeea63
fasasi.azeez@yahoo.com <fasasi.azeez@yahoo.com>
2023-05-31 09:55:36 +0100

    Files Added

---
+++ js/button.js
@@ -0,0 +1,32 @@
+var myHtml = document.getElementById("html");
+var myCss = document.getElementById("css");
+var myJs = document.getElementById("js");
+var myWp = document.getElementById("wp")
+var myExcel = document.getElementById("excel");
+var myGs = document.getElementById("gs");
+
+
+myHtml.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20HTML%20course.%20%20My%20name%20is");
+});
+
+myCss.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20CSS%20course.%20%20My%20name%20is");
+});
+
+myJs.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20JavaScript%20course.%20%20My%20name%20is");
+});
+
+myWp.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20WordPress%20course.%20%20My%20name%20is");
+});
+
+myExcel.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20Excel%20course.%20%20My%20name%20is");
+});
+
+myGs.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20Google%20Sheets%20course.%20%20My%20name%20is");
+});
+
---
+++ js/courses.js
@@ -0,0 +1,42 @@
+var myHtml1 = document.getElementById("c1");
+var myCss2 = document.getElementById("c2");
+var myJs3 = document.getElementById("c3");
+var myWp4 = document.getElementById("c4")
+var myExcel5 = document.getElementById("c5");
+var myGs6 = document.getElementById("c6");
+
+
+
+myHtml1.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20HTML%20course.%20%20My%20name%20is");
+});
+
+myCss2.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20CSS%20course.%20%20My%20name%20is");
+});
+
+myJs3.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20JavaScript%20course.%20%20My%20name%20is");
+});
+
+myWp4.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20WordPress%20course.%20%20My%20name%20is");
+});
+
+myExcel5.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20Excel%20course.%20%20My%20name%20is");
+});
+
+myGs6.addEventListener("click", function() {
+  window.open("https://wa.me/08117256648?text=Hello%20Azeez,%20I'm%20interested%20in%20the%20Google%20Sheets%20course.%20%20My%20name%20is");
+});
+
+
+
+
+
+
+
+
+
+
