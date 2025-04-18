11f5db7460003bf73b5d5291455312b07b0d0629
azeezfasasi <fas.azeez@gmail.com>
2025-04-19 00:49:00 +0100

    Okay

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://sense-academy-backend.onrender.com' // Hosted backend
-  : 'http://localhost:5000'; // Local backend
+// const API_BASE_URL = process.env.NODE_ENV === 'production'
+//   ? 'https://sense-academy-backend.onrender.com' // Hosted backend
+//   : 'http://localhost:5000'; // Local backend
+
+// export default API_BASE_URL;
+
+const API_BASE_URL = 'https://sense-academy-backend.onrender.com';
 
 export default API_BASE_URL;
